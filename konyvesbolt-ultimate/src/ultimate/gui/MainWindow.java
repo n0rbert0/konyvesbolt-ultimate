d8819353f5a94cfb964270fd3f26c7e77055032b
Tamas Czene <tczene@inf.u-szeged.hu>
2012-04-10 15:09:59 +0200

    MainWindow

---
+++ konyvesbolt-ultimate/src/ultimate/gui/MainWindow.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimate.gui;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author lorda
+ */
+public class MainWindow extends JPanel{
+    
+    
+    
+}
