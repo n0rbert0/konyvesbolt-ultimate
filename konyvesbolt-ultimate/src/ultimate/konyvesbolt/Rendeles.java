cc7a2d2e4f2e9652a1a1d026bf0c5f6555f873e3
n0rbert0 <petaknorbi@hotmail.com>
2012-05-01 21:41:55 +0200

    Rendeles osztaly letrehozasa

--- konyvesbolt-ultimate/src/ultimate/konyvesbolt/Rendeles.java
+++ konyvesbolt-ultimate/src/ultimate/konyvesbolt/Rendeles.java
@@ -10,94 +10,114 @@ package ultimate.konyvesbolt;
  */
 public class Rendeles {
     
-    private int R_ID;
-    private String Varos, Utca;
-    private int Hazszam, IrSzam;
-    private Konyv RendeltKonyv; //ebből egyértelmű a könyv címe másik tábla alapján
-    private int RendeltKönyvDarabSzam;
-    private int[] HozzaISBNek; //"rendelt darabszám" hosszú tömb, amiben az ISBN-ek vannak.
-    private boolean Kiszallitott = false; //igaz, ha már kiszállításra került
-
+    private int r_id;
+    private int f_id;
+    private int isbn;
+    private int db;
+    private String datum;
+    private int ar;
     
-    //Getters: 
-    public int getHazszam() {
-        return Hazszam;
+    private int irSzam;
+    private String varos;
+    private String utca;
+    private int hazszam;
+    
+    private boolean kiszalitva = false;
+
+    public int getAr() {
+        return ar;
     }
 
-    public int[] getHozzaISBNek() {
-        return HozzaISBNek;
+    public String getDatum() {
+        return datum;
+    }
+
+    public int getDb() {
+        return db;
+    }
+
+    public int getF_id() {
+        return f_id;
+    }
+
+    public int getHazszam() {
+        return hazszam;
     }
 
     public int getIrSzam() {
-        return IrSzam;
+        return irSzam;
     }
 
-    public boolean isKiszallitott() {
-        return Kiszallitott;
+    public int getIsbn() {
+        return isbn;
     }
 
-    public int getR_ID() {
-        return R_ID;
+    public boolean isKiszalitva() {
+        return kiszalitva;
     }
 
-    public Konyv getRendeltKonyvCim() {
-        return RendeltKonyv;
-    }
-
-    public int getRendeltKönyvDarabSzam() {
-        return RendeltKönyvDarabSzam;
+    public int getR_id() {
+        return r_id;
     }
 
     public String getUtca() {
-        return Utca;
+        return utca;
     }
 
     public String getVaros() {
-        return Varos;
-    }
-    
-    //Setters: 
-
-    public void setHazszam(int Hazszam) {
-        this.Hazszam = Hazszam;
+        return varos;
     }
 
-    public void setHozzaISBNek(int[] HozzaISBNek) {
-        this.HozzaISBNek = HozzaISBNek;
+    public void setAr(int ar) {
+        this.ar = ar;
     }
 
-    public void setIrSzam(int IrSzam) {
-        this.IrSzam = IrSzam;
+    public void setDatum(String datum) {
+        this.datum = datum;
     }
 
-    public void setKiszallitott(boolean Kiszallitott) {
-        this.Kiszallitott = Kiszallitott;
+    public void setDb(int db) {
+        this.db = db;
     }
 
-    public void setR_ID(int R_ID) {
-        this.R_ID = R_ID;
+    public void setF_id(int f_id) {
+        this.f_id = f_id;
     }
 
-    public void setRendeltKonyv(Konyv RendeltKonyv) {
-        this.RendeltKonyv = RendeltKonyv;
+    public void setHazszam(int hazszam) {
+        this.hazszam = hazszam;
     }
 
-    public void setRendeltKönyvDarabSzam(int RendeltKönyvDarabSzam) {
-        this.RendeltKönyvDarabSzam = RendeltKönyvDarabSzam;
+    public void setIrSzam(int irSzam) {
+        this.irSzam = irSzam;
     }
 
-    public void setUtca(String Utca) {
-        this.Utca = Utca;
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
     }
 
-    public void setVaros(String Varos) {
-        this.Varos = Varos;
+    public void setKiszalitva(boolean kiszalitva) {
+        this.kiszalitva = kiszalitva;
     }
 
-    
-    
-    
-    
-    
+    public void setR_id(int r_id) {
+        this.r_id = r_id;
+    }
+
+    public void setUtca(String utca) {
+        this.utca = utca;
+    }
+
+    public void setVaros(String varos) {
+        this.varos = varos;
+    }
+
+    @Override
+    public String toString() {
+        return " {R_ID=" + r_id + ", F_ID=" + f_id + ", ISBN=" + isbn +
+               ", DB=" + db + ", DATUM=" + datum + ", AR=" + ar + ", IRSZAM=" +
+               irSzam + ", VAROS=" + varos + ", UTCA=" + utca + ", HAZSZAM="
+               + hazszam + ", KISZALITVA=" + kiszalitva + '}';
+    }
     
 }//class
