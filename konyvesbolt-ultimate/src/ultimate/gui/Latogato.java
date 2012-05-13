acbbf8b9ad81d4c606cbade8be07591c5ccec3b0
Tamas Czene <tczene@inf.u-szeged.hu>
2012-05-13 19:34:37 +0200

    1

--- konyvesbolt-ultimate/src/ultimate/gui/Latogato.java
+++ konyvesbolt-ultimate/src/ultimate/gui/Latogato.java
@@ -344,11 +344,11 @@ public class Latogato extends javax.swing.JPanel {
             		    "Információ",
             		    JOptionPane.INFORMATION_MESSAGE);
                 }else if(dao.belepes(felhasz.getText(), pass.getText()) != null &&
-                   dao.belepes(felhasz.getText(), pass.getText()).getJog() == 1){
-                    JOptionPane.showMessageDialog(null,
-            		    "Még nincs kész az admin felület!\nTürelem...",
-            		    "Hiba",
-            		    JOptionPane.ERROR_MESSAGE);
+                    dao.belepes(felhasz.getText(), pass.getText()).getJog() == 1){
+                    setVisible(false);                    
+                    foablak.add(new admin());
+                    felhasz.setText("Felhasználónév");
+                    pass.setText("jPasswordField1");
                 }else{
                     JOptionPane.showMessageDialog(null,
             		    "Hibás felhasználónév vagy jelszó!",
